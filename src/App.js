25d12e4be7d167c7516f17fcade10559981a961c
india@1410 <prakashsingh20497@gmail.com>
2023-08-02 16:46:27 +0530

    added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Booking from './Components/Booking';
+import BookingDataPage from './Components/BookingData';
+
+function App() {
+  return (
+    <div className="App">
+      <Booking/>
+     
+    </div>
+  );
+}
+
+export default App;
